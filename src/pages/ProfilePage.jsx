007d83edce0e3a1a26ed00bd582cf9960fbb21d7
bhanu2006-24 <bhanupsaini2024@gmail.com>
2026-02-02 20:37:14 +0530

    fixed some bugs

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import { useApp } from '../context/AppContext';
 import { motion, AnimatePresence } from 'framer-motion';
